ce541080226f9014e1dc361e0d69ae6d5aa992aa
Sumiitt-007 <sumitrpt007@gmail.com>
2025-08-10 23:48:23 +0530

    Recursion...

---
+++ src/Recursion/CombinationSum.java
@@ -0,0 +1,19 @@
+package Recursion;
+import java.util.*;
+
+public class CombinationSum {
+    public static void main(String[] args) {
+        int[] candidates = {2,3,6,7};
+        int target = 7;
+    }
+//    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//
+//    }
+//    private List<List<Integer>> helper(int[] arr, int target, int index, int sum,List<List<Integer>> outer){
+//        if (index == arr.length){
+//            if (sum == target){
+//
+//            }
+//        }
+//    }
+}
--- src/Recursion/Subsequence.java
+++ src/Recursion/Subsequence.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Subsequence {
+    private static int count =0;
     public static void main(String[] args) {
-        int[] arr = {1,2,3};
-        Subseq(0,arr,new ArrayList<>());
+        int[] arr = {1,2,3,1,2,6};
+//        Subseq(0,arr,new ArrayList<>());
+        targetSumSubSeq(arr,0,new ArrayList<>(),0, 4);
+        System.out.println(count);
     }
+    // Print all the subsequences
     private static void Subseq(int index, int[] arr, List<Integer> list){
         if (index  > arr.length - 1){
             System.out.println(list);
@@ -17,5 +21,25 @@ public class Subsequence {
         Subseq(index + 1, arr, list);
         list.remove(list.size() - 1);
         Subseq(index +1 ,arr,list);
+    }
+    // print the subsequences with Target Sum
+
+    static void targetSumSubSeq(int[] arr, int ind, List<Integer> list, int sum, int target){
+        if (ind == arr.length){
+            if (sum == target){
+                count++;
+                System.out.println(list);
+            }
+            return;
+        }
+        // Pick up the element
+        list.add(arr[ind]);
+        sum += arr[ind];
+        targetSumSubSeq(arr,ind + 1,list,sum,target);
+
+        // Don't pick up the element
+        list.remove(list.size() - 1);
+        sum -= arr[ind];
+        targetSumSubSeq(arr,ind + 1,list,sum,target);
     }
 }
